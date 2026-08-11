31b24f88d9539f696d6378d702b72ab920dccb77
bulletmummy <katariyaglobal@gmail.com>
2026-08-11 12:25:34 +0530

    Update chrome_startup.ps1

--- scripts/chrome_startup.ps1
+++ scripts/chrome_startup.ps1
@@ -1,3 +1,12 @@
+# Set wallpaper
+$wp = "C:\Windows\Temp\bullet_wallpaper.png"
+if (Test-Path $wp) {
+    $code = 'using System; using System.Runtime.InteropServices; public class WP2 { [DllImport("user32.dll",CharSet=CharSet.Auto)] public static extern int SystemParametersInfo(int a,int b,string c,int d); }'
+    Add-Type -TypeDefinition $code -Language CSharp -ErrorAction SilentlyContinue
+    [WP2]::SystemParametersInfo(20, 0, $wp, 3) | Out-Null
+}
+
+# Launch Chrome
 $chrome = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 if (Test-Path $chrome) {
     Start-Sleep -Seconds 3
